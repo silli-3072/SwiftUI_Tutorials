2a771f9cea0c90ac939f1daea0b367a49407d563
silli-3072 <hrtmnr.3072@gmail.com>
2023-05-11 11:39:59 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 春田実利 on 2023/05/11.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
